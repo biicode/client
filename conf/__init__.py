ccf02c5e01d03c25a0ad0842a961e4dd6945d28e
Luis Martínez de Bartolomé Izquierdo <lasote@gmail.com>
2015-04-07 15:32:35 +0200

    Removed unused code

--- conf/__init__.py
+++ conf/__init__.py
@@ -4,6 +4,3 @@ from biicode.common.conf.configure_environment import get_env
 from biicode.common.conf import MEGABYTE
 
 BII_RESTURL = get_env('BII_RESTURL', 'https://biiserverproduction.herokuapp.com')
-# web url, for loading resources, such as javascript and css files
-BII_WEBURL = get_env('BII_WEBURL', 'http://www.biicode.com')
-BII_WEBSTATIC = get_env('BII_WEBSTATIC', 'https://biiwebproduction.s3.amazonaws.com')
